1d2e84b161ffd2d5dd031f81e6c8dc184cce0521
fiki2002 <adepitanoluwatosin202@gmail.com>
2022-08-06 10:29:12 +0100

    Added the colors to be used

---
+++ lib/styles/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = const Color(0xff742DDD);
+
+Color lightAmber = const Color(0xffFFF9E7);
+
+Color amber = const Color(0xffEDB200);
+
+Color green = const Color(0xff009120);
+
+Color grey = const 
