4623263a020769a4f166cd5e20b0e922f322f4b3
fiki2002 <adepitanoluwatosin202@gmail.com>
2022-08-06 10:38:54 +0100

    Added routing

---
+++ lib/utils/routers.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PageNavigator {
+  BuildContext? ctext;
+  PageNavigator({this.ctext});
+
+  Future nextPage({Widget? page}) {
+    return Navigator.push(
+        ctext!, CupertinoPageRoute(builder: ((context) => page!)));
+  }
+
+  void nextPageOnly({Widget? page}) {
+    Navigator.pushAndRemoveUntil(ctext!,
+        MaterialPageRoute(builder: (context) => page!), (route) => false);
+  }
+}
